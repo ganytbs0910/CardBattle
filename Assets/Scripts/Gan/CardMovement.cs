ebb97df30da5fe569b4e0ca7a01aa13297c4ecd1
gan <103314509+ganytbs0910@users.noreply.github.com>
2023-12-19 15:59:18 +0900

    Discordで反映されるのか...?

--- Assets/Scripts/Gan/CardMovement.cs
+++ Assets/Scripts/Gan/CardMovement.cs
@@ -2,41 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using DG.Tweening;
 
 public class CardMovement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    [SerializeField] private Toggle toggle;
     public Transform cardParent;
-    public bool isDrag = false;
-
-    void Start()
-    {
-        //この親オブジェクトのToggleGroupの中にToggleを入れる
-        toggle.group = GetComponentInParent<ToggleGroup>();
-
-        toggle.onValueChanged.AddListener(OnToggleChanged);
-        // DOTweenアニメーションの追加
-        transform.DORotate(new Vector3(0f, 360f, 0f), 1f, RotateMode.FastBeyond360)
-            .SetEase(Ease.Linear); // 1秒で360度回転
-    }
 
     public void OnBeginDrag(PointerEventData eventData) // ドラッグを始めるときに行う処理
     {
-        if (!isDrag) return;
-        // Toggleのチェックマークをオンにする
-        toggle.isOn = true;
         cardParent = transform.parent;
-        Transform newParent = cardParent.parent.parent; // さらに一つ上の階層を親オブジェクトに設定
-        if (newParent != null) // 新しい親が存在するか確認
-        {
-            transform.SetParent(newParent, false);
-        }
+        transform.SetParent(cardParent.parent, false);
         GetComponent<CanvasGroup>().blocksRaycasts = false; // blocksRaycastsをオフにする
     }
 
-    public void OnDrag(PointerEventData eventData) // ドラッグしてる時に起こす処理
+    public void OnDrag(PointerEventData eventData) // ドラッグした時に起こす処理
     {
         transform.position = eventData.position;
     }
@@ -45,23 +23,5 @@ public class CardMovement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEnd
     {
         transform.SetParent(cardParent, false);
         GetComponent<CanvasGroup>().blocksRaycasts = true; // blocksRaycastsをオンにする
-    }
-
-    private void OnToggleChanged(bool isOn)
-    {
-        if (isOn)
-        {
-            // サイズを1.2倍に拡大
-            transform.DOScale(1.1f, 0.1f);
-
-            // その他の処理
-            CardController cardController = GetComponent<CardController>();
-            CardModel cardModel = cardController.model;
-        }
-        else
-        {
-            // サイズを元に戻す
-            transform.DOScale(1f, 0.1f);
-        }
     }
 }
--- Assets/Scripts/Gan/DropPlace.cs
+++ Assets/Scripts/Gan/DropPlace.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 // フィールドにアタッチするクラス
 public class DropPlace : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData) // ドロップされた時に行う処理
     {
-        CardMovement card = eventData.pointerDrag.GetComponent<CardMovement>();
+        CardMovement card = eventData.pointerDrag.GetComponent<CardMovement>(); // ドラッグしてきた情報からCardMovementを取得
         if (card != null) // もしカードがあれば、
         {
             card.cardParent = this.transform; // カードの親要素を自分（アタッチされてるオブジェクト）にする
