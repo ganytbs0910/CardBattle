b6a6efb85d93eb03325f03a719569592c7d8d8a1
ChimidoroGekidan <chimidorogekidan@gmail.com>
2023-12-20 00:16:12 +0900

    Dungeonアセットの追加
    Dungeonアセットを追加しました

--- Assets/Scripts/Wonka/PlayerController.cs
+++ Assets/Scripts/Wonka/PlayerController.cs
@@ -11,14 +11,69 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int defense;
     [SerializeField] private int speed;
 
+    private Animator animator;
+
+    public static PlayerController instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
     void Start()
     {
-
+        // Animatorコンポーネントを取得
+        animator = GetComponent<Animator>();
     }
 
-    void Update()
+    // 各アニメーション状態をトリガーするメソッド
+    public void Move()
     {
+        animator.SetTrigger("Move");
+    }
 
+    public void Idle()
+    {
+        animator.SetTrigger("Idle");
+    }
+
+    public void Attack()
+    {
+        int attackNumber = Random.Range(1, 5); // 1から4の間のランダムな数を生成
+
+        // 生成された数に応じて異なる攻撃アニメーションをトリガー
+        switch (attackNumber)
+        {
+            case 1:
+                animator.SetTrigger("Attack01");
+                break;
+            case 2:
+                animator.SetTrigger("Attack02");
+                break;
+            case 3:
+                animator.SetTrigger("Attack03");
+                break;
+            case 4:
+                animator.SetTrigger("Attack04");
+                break;
+        }
+    }
+
+    public void GetHit()
+    {
+        animator.SetTrigger("GetHit");
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger("Die");
+    }
+
+    public void Victory()
+    {
+        animator.SetTrigger("Victory");
     }
 }
