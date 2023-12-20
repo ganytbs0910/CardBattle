99023d5d63d32f730b83b5e63302fd5b5ca3a205
ChimidoroGekidan <chimidorogekidan@gmail.com>
2023-12-20 15:17:16 +0900

    UTF8テスト
    文字化けしてないか見てちょ

---
+++ Assets/Scripts/Wonka/TestUTF8.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUTF8 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //文字化けしてたら連絡してください
+        print("がんさん見てるー！？");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
