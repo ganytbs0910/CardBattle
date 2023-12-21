cef46eefcd21caa84a73203edcdf39613c982338
ganytbs0910 <nakakou2002@icloud.com>
2023-12-21 19:33:00 +0900

    Pull用のCommit

--- Assets/Scripts/Gan/ToggleAnimation.cs
+++ Assets/Scripts/Gan/ToggleAnimation.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,6 +12,7 @@ public class ToggleAnimation : MonoBehaviour
     {
         toggle = GetComponent<Toggle>();
         toggle.onValueChanged.AddListener(OnToggleChanged);
+        OnToggleChanged(toggle.isOn);
     }
 
     void Update()
@@ -23,15 +24,25 @@ public class ToggleAnimation : MonoBehaviour
     {
         if (isOn)
         {
-            //サイズを1.1倍にして色を白にする
-            transform.DOScale(1.1f, 0.2f);
+            //サイズをyだけ1.1倍にして色を白にする
+            transform.DOScale(new Vector3(1f, 1.3f, 1f), 0.2f);
             GetComponent<Image>().DOColor(Color.white, 0.2f);
+            //このオブジェクトの子オブジェクトのImageを取得してサイズを1.1倍にする
+            foreach (Transform child in transform)
+            {
+                child.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 0.2f);
+            }
         }
         else
         {
             //元に戻す
-            transform.DOScale(1f, 0.2f);
+            transform.DOScale(new Vector3(1f, 1f, 1f), 0.2f);
             GetComponent<Image>().DOColor(Color.gray, 0.2f);
+            //このオブジェクトの子オブジェクトのImageを取得して元通りにする
+            foreach (Transform child in transform)
+            {
+                child.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 0.2f);
+            }
         }
     }
 }
