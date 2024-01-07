430fd2283727f21c9934624869292565dcb80a94
ganytbs0910 <nakakou2002@icloud.com>
2024-01-07 20:11:48 +0900

    PlayFabのSDKもんだいかいけつ

---
+++ Assets/Scripts/Gan/PlayFabLogin.cs
@@ -0,0 +1,28 @@
+﻿using PlayFab;//追加
+using PlayFab.ClientModels;//追加
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    public void Start()
+    {
+        //リクエスト生成
+        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+
+        //ログイン処理実行
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    //ログイン成功時に呼び出される関数
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("ログイン成功！！");
+    }
+
+    //ログイン失敗時に呼び出される関数
+    private void OnLoginFailure(PlayFabError error)
+    {        
+        Debug.LogError("ログインに失敗しました。");
+        Debug.LogError(error.GenerateErrorReport());//エラー内容
+    }
+}
