1aac085c46e646d0db4f6596b0fd59484de41d30
ganytbs0910 <nakakou2002@icloud.com>
2024-01-17 13:59:18 +0900

    カードの購入

--- Assets/Scripts/Gan/DrawCardController.cs
+++ Assets/Scripts/Gan/DrawCardController.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class DrawCardController : MonoBehaviour
 {
+    public int maximumCardNumber = 8;
     public static DrawCardController instance;
 
     public Image parentPanel;
@@ -28,7 +29,6 @@ public class DrawCardController : MonoBehaviour
             {
                 DrawCard();
             }
-
         }
         else
         {
@@ -40,7 +40,7 @@ public class DrawCardController : MonoBehaviour
 #endif
 
 #if UNITY_EDITOR
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < maximumCardNumber; i++)
         {
             DrawCard();
         }
@@ -52,10 +52,12 @@ public class DrawCardController : MonoBehaviour
 
     }
 
+
+
     public void DrawCard(int? cardID = null)
     {
-        int tiar = CalculateTiar();
-        if (parentPanel.transform.childCount > 8) return;
+        if (parentPanel.transform.childCount >= maximumCardNumber) return;
+        int tiar = GameManager.instance.CalculateTiar();
 
         // カードIDが指定されていない場合、ランダムにカードを選択
         if (cardID == null)
@@ -100,57 +102,6 @@ public class DrawCardController : MonoBehaviour
             TiarSelectOutline(card, cardModel);
         }
     }
-
-    //階層ごとのカードの確率
-    int CalculateTiar()
-    {
-        int stageHierarchy = GameManager.instance.stageHierarchy;
-        // 確率を格納する配列
-        float[] probabilities;
-
-        // Stageに応じた確率を設定
-        if (stageHierarchy <= 10)
-        {
-            // Stage 1~10の確率
-            probabilities = new float[] { 0.6f, 0.2f, 0.18f, 0.1f, 0.05f, 0.02f };
-        }
-        else if (stageHierarchy <= 20)
-        {
-            // Stage 11~20の確率
-            probabilities = new float[] { 0.4f, 0.2f, 0.2f, 0.1f, 0.05f, 0.05f };
-        }
-        else if (stageHierarchy <= 30)
-        {
-            // Stage 21~30までの確率
-            probabilities = new float[] { 0.2f, 0.2f, 0.2f, 0.2f, 0.1f, 0.1f };
-        }
-        else if (stageHierarchy <= 40)
-        {
-            // Stage 31~40までの確率
-            probabilities = new float[] { 0.1f, 0.15f, 0.2f, 0.25f, 0.1f, 0.2f };
-        }
-        else
-        {
-            // デフォルトの確率（必要に応じて設定）
-            probabilities = new float[] { 0.1f, 0.2f, 0.3f, 0.2f, 0.1f, 0.1f };
-        }
-
-        // 乱数を生成してtiarを決定
-        float randomValue = UnityEngine.Random.value;
-        float cumulativeProbability = 0.0f;
-        for (int tiar = 1; tiar <= probabilities.Length; tiar++)
-        {
-            cumulativeProbability += probabilities[tiar - 1];
-            if (randomValue < cumulativeProbability)
-            {
-                return tiar;
-            }
-        }
-
-        return 1; // 万が一、どの条件にも当てはまらない場合は最低値を返す
-    }
-
-
 
     //ランクに応じてアウトライン変更
     public void TiarSelectOutline(CardController card, CardModel cardModel)
