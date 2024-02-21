553853bb408fcc79e20ccdde045c260dad7cee40
ganytbs0910 <nakakou2002@icloud.com>
2024-02-21 13:59:10 +0900

    音量のセーブ機能が少しバグっていたのを修正

--- Assets/Scripts/Wonka/MixerController.cs
+++ Assets/Scripts/Wonka/MixerController.cs
@@ -9,29 +9,26 @@ public class MixerController : MonoBehaviour
 
     void Start()
     {
-        // ゲーム開始時に各スライダーを初期化し、AudioMixerと連動させ、PlayerPrefsから値を取得して設定する
-        InitializeSlider(masterSlider, "MasterVolume", 0.5f); // MasterVolumeパラメータに関連付ける
-        InitializeSlider(bgmSlider, "BGMVolume", 1); // BGMVolumeパラメータに関連付ける
-        InitializeSlider(seSlider, "SEVolume", 1); // SEVolumeパラメータに関連付ける
-        InitializeSlider(voiceSlider, "VoiceVolume", 1); // VoiceVolumeパラメータに関連付ける
+        // ゲーム開始時に各スライダーを初期化し、AudioMixerと連動させる
+        InitializeSlider(masterSlider, "Master", PlayerPrefs.GetFloat("Master")); // MasterVolumeパラメータに関連付ける
+        InitializeSlider(bgmSlider, "BGM", PlayerPrefs.GetFloat("BGM")); // BGMVolumeパラメータに関連付ける
+        InitializeSlider(seSlider, "SE", PlayerPrefs.GetFloat("SE")); // SEVolumeパラメータに関連付ける
+        InitializeSlider(voiceSlider, "Voice", PlayerPrefs.GetFloat("Voice")); // VoiceVolumeパラメータに関連付ける
     }
 
     void InitializeSlider(Slider slider, string audioMixerParameter, float defaultValue)
     {
-        // PlayerPrefsからボリューム値を読み込み、存在しない場合はdefaultValueを使用
-        float savedValue = PlayerPrefs.GetFloat(audioMixerParameter, defaultValue);
-        // スライダーの値を設定
-        slider.value = savedValue;
+        // スライダーの初期値を設定
+        slider.value = defaultValue;
         // スライダーの値が変更されたときのリスナーを設定
         slider.onValueChanged.AddListener(volume =>
-        {
-            Debug.Log(audioMixerParameter + " : " + volume);
-            SetVolume(audioMixerParameter, volume);
-            // 新しいボリューム値をPlayerPrefsに保存
-            PlayerPrefs.SetFloat(audioMixerParameter, volume);
-        });
+    {
+        SetVolume(audioMixerParameter, volume);
+        // 新しいボリューム値をPlayerPrefsに保存
+        PlayerPrefs.SetFloat(audioMixerParameter, volume);
+    });
         // AudioMixerに初期値を適用
-        SetVolume(audioMixerParameter, savedValue);
+        SetVolume(audioMixerParameter, defaultValue);
     }
 
     void SetVolume(string parameterName, float sliderValue)
